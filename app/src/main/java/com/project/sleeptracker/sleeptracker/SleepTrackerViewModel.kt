98f9c18efeb0c7e616d7ed99846660013c5ebe90
Livia Carvalho <livia.carvalho.lb@gmail.com>
2024-03-04 20:29:52 -0400

    added a new ViewHolder

--- app/src/main/java/com/project/sleeptracker/sleeptracker/SleepTrackerViewModel.kt
+++ app/src/main/java/com/project/sleeptracker/sleeptracker/SleepTrackerViewModel.kt
@@ -1,7 +1,6 @@
 package com.project.sleeptracker.sleeptracker
 
 import android.app.Application
-import android.view.animation.Transformation
 import androidx.lifecycle.AndroidViewModel
 import androidx.lifecycle.LiveData
 import androidx.lifecycle.MutableLiveData
